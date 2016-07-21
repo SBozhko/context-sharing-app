10d7f7ef93172f5d00ab09dd70a03cf06bf1e472
senabhishek <sen.abhishek@gmail.com>
2016-07-21 23:45:31 +0100

    Updated controls based on views. Testing

--- AllAboutMe/MeViewController.swift
+++ AllAboutMe/MeViewController.swift
@@ -16,8 +16,12 @@ import MessageUI
 import Toast
 
 class MeViewController: UIViewController {
-  @IBOutlet weak var situationCollectionView: UICollectionView!
   @IBOutlet weak var otherContextCollectionView: UICollectionView!
+  @IBOutlet weak var situationView: UIView!
+  @IBOutlet weak var situationImageView: UIImageView!
+  @IBOutlet weak var situationLabel: UILabel!
+  
+  
   var uniqueMessageCode : NSString = ""
   let messageSubjectCodeLength = 10
 
@@ -28,6 +32,7 @@ class MeViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    NELogging.dumpLogs()
     // Do any additional setup after loading the view, typically from a nib.
     NSNotificationCenter.defaultCenter().addObserver(self,
                                                      selector: #selector(MeViewController.handleUserContextOverrideTimerExpired(_:)),
@@ -37,6 +42,9 @@ class MeViewController: UIViewController {
                                                      selector: #selector(MeViewController.handleProfileIdReceivedNotification(_:)),
                                                      name: profileIdReceivedNotification,
                                                      object: nil)
+    let situationImagetapGestureRecognizer = UITapGestureRecognizer(target:self, action: #selector(MeViewController.situationImageViewTapped))
+    situationImageView.userInteractionEnabled = true
+    situationImageView.addGestureRecognizer(situationImagetapGestureRecognizer)
   }
 
   override func viewWillAppear(animated: Bool) {
@@ -46,7 +54,7 @@ class MeViewController: UIViewController {
         print("\(NEDayCategory.get()!.name.name): \(context.name)-\(context.group.name)")
         dispatch_async(dispatch_get_main_queue(), {
           if context.group == NEContextGroup.Situation {
-            self.situationCollectionView.reloadData()
+            self.updateSituationView()
           } else {
             self.otherContextCollectionView.reloadData()
           }
@@ -55,7 +63,7 @@ class MeViewController: UIViewController {
       })
       initializeContexts()
     } else {
-      self.situationCollectionView.reloadData()
+      self.updateSituationView()
       self.otherContextCollectionView.reloadData()
     }
   }
@@ -67,13 +75,17 @@ class MeViewController: UIViewController {
   
   func handleUserContextOverrideTimerExpired(notification : NSNotification) {
     if self.isViewVisible() && UIApplication.sharedApplication().applicationState == .Active {
-      self.situationCollectionView.reloadData()
+      self.updateSituationView()
       self.otherContextCollectionView.reloadData()
     }
   }
   
   func handleProfileIdReceivedNotification(notification : NSNotification) {
     initializeContexts()
+  }
+  
+  func situationImageViewTapped() {
+    selectedContext(NEContextGroup.Situation)
   }
   
   func initializeContexts() {
@@ -107,6 +119,21 @@ class MeViewController: UIViewController {
             print("JSON: \(JSON)")
           }
       }
+    }
+  }
+  
+  func updateSituationView() {
+    let currentContext = ContextInfo.sharedInstance.getValidCurrentContext(NESituation.group)
+    if let
+      validContextName = currentContext.context,
+      validContextImageName = currentContext.imageName {
+      //      Send request for image
+      situationImageView.image = UIImage(named: validContextImageName)
+      situationLabel.text = ContextInfo.sharedInstance.getSituationDisplayMessage(validContextName.name)
+    } else {
+      //      Show loading image
+      situationImageView.image = UIImage(named: "unknown")
+      situationLabel.text = NESituation.group.name
     }
   }
   
@@ -172,7 +199,7 @@ class MeViewController: UIViewController {
               }
               dispatch_async(dispatch_get_main_queue(), {
                 if _context.group == NEContextGroup.Situation {
-                  self.situationCollectionView.reloadData()
+                  self.updateSituationView()
                 } else {
                   self.otherContextCollectionView.reloadData()
                 }
@@ -192,7 +219,7 @@ class MeViewController: UIViewController {
               }
               dispatch_async(dispatch_get_main_queue(), {
                 if _contextGroup == NEContextGroup.Situation {
-                  self.situationCollectionView.reloadData()
+                  self.updateSituationView()
                 } else {
                   self.otherContextCollectionView.reloadData()
                 }
@@ -204,96 +231,30 @@ class MeViewController: UIViewController {
       }
     }
   }
-}
-
-extension MeViewController : MFMailComposeViewControllerDelegate {
-  func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
-    controller.dismissViewControllerAnimated(true, completion: nil)
-    if result == MFMailComposeResultSent {
-      dispatch_async(dispatch_get_main_queue(), { () -> Void in
-        self.view.makeToast("Thanks for reporting!", duration: 3.0, position: CSToastPositionBottom, style: nil)
-      })
-      Logging.sharedInstance.userInitiatedLogDump(uniqueMessageCode as String)
-    }
-  }
-}
-
-extension MeViewController : UIPopoverPresentationControllerDelegate {
-  func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
-    return UIModalPresentationStyle.OverFullScreen
-  }
   
-  func popoverPresentationControllerShouldDismissPopover(popoverPresentationController: UIPopoverPresentationController) -> Bool {
-    return true
-  }
-}
-
-extension MeViewController : UICollectionViewDelegate, UICollectionViewDataSource {
-  func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-    let contextGroupCellIndex = collectionView == otherContextCollectionView ? indexPath.row+1 : 0
+  func selectedContext(contextGroup : NEContextGroup) {
     let storyboard = UIStoryboard(name: "Main", bundle: nil)
-    let overriddenContext = ContextInfo.sharedInstance.getOverriddenContext(contextGroupCells[contextGroupCellIndex])
+    let overriddenContext = ContextInfo.sharedInstance.getOverriddenContext(contextGroup)
     if let vc = storyboard.instantiateViewControllerWithIdentifier("ContextPopoverViewController") as? ContextPopoverViewController {
       vc.modalPresentationStyle = .Popover
       let popover = vc.popoverPresentationController!
       popover.delegate = self
       popover.sourceView = self.view
       if overriddenContext.flag {
-        vc.overriddenContextGroup = contextGroupCells[contextGroupCellIndex]
+        vc.overriddenContextGroup = contextGroup
         if let _contextName = overriddenContext.contextName {
           vc.overriddenContextName = _contextName
         } else if let _userEnteredContextString = overriddenContext.userContextString {
           vc.overriddenUserEnteredContextString = _userEnteredContextString
         }
-      } else if let currentContext = ContextInfo.sharedInstance.getCurrentContext(contextGroupCells[contextGroupCellIndex]).context {
+      } else if let currentContext = ContextInfo.sharedInstance.getCurrentContext(contextGroup).context {
         vc.context = currentContext
       }
       presentViewController(vc, animated: true, completion: nil)
     }
   }
   
-  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! MeCollectionViewCell
-    let contextGroupCellIndex = collectionView == otherContextCollectionView ? indexPath.row+1 : 0
-    let contextGroup = contextGroupCellIndex == 0 ? NEContextGroup.Situation : contextGroupCells[contextGroupCellIndex]
-    let overriddenContext = ContextInfo.sharedInstance.getOverriddenContext(contextGroup)
-    if overriddenContext.flag {
-      if let contextName = overriddenContext.contextName {
-        cell.imageView.image = UIImage(named: contextName.name.lowercaseString)
-        cell.contextLabel.text = contextGroup == NEContextGroup.Situation ?
-          ContextInfo.sharedInstance.getSituationDisplayMessage(contextName) :
-          contextName.name
-      } else if let userContextString = overriddenContext.userContextString {
-        cell.contextLabel.text = userContextString
-        cell.imageView.image = UIImage(named: "unknown")
-      } else {
-        //      Show loading image
-        cell.imageView.image = UIImage(named: "unknown")
-        cell.contextLabel.text = contextGroup.name
-      }
-    } else {
-      let currentContext = ContextInfo.sharedInstance.getValidCurrentContext(contextGroup)
-      if let
-        validContextName = currentContext.context,
-        validContextImageName = currentContext.imageName {
-        //      Send request for image
-        cell.imageView.image = UIImage(named: validContextImageName)
-        cell.contextLabel.text = contextGroup == NEContextGroup.Situation ?
-          ContextInfo.sharedInstance.getSituationDisplayMessage(validContextName.name) :
-          validContextName.name.name
-      } else {
-        //      Show loading image
-        cell.imageView.image = UIImage(named: "unknown")
-        cell.contextLabel.text = contextGroup.name
-      }
-    }
-    if collectionView == situationCollectionView {
-      cell.shareButton.addTarget(self, action: #selector(MeViewController.shareButtonPressed(_:)), forControlEvents: UIControlEvents.TouchUpInside)
-    }
-    return cell
-  }
-  
-  @IBAction func shareButtonPressed(sender: AnyObject) {
+  @IBAction func shareButtonPressed() {
     let optionMenu = UIAlertController(title: nil, message: nil, preferredStyle: UIAlertControllerStyle.ActionSheet)
     let twitterAction = UIAlertAction(title: "Twitter", style: .Default, handler: {
       (alert: UIAlertAction!) -> Void in
@@ -322,23 +283,78 @@ extension MeViewController : UICollectionViewDelegate, UICollectionViewDataSourc
     optionMenu.addAction(cancelAction)
     self.presentViewController(optionMenu, animated: true, completion: nil)
   }
+}
+
+extension MeViewController : MFMailComposeViewControllerDelegate {
+  func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+    controller.dismissViewControllerAnimated(true, completion: nil)
+    if result == MFMailComposeResultSent {
+      dispatch_async(dispatch_get_main_queue(), { () -> Void in
+        self.view.makeToast("Thanks for reporting!", duration: 3.0, position: CSToastPositionBottom, style: nil)
+      })
+      Logging.sharedInstance.userInitiatedLogDump(uniqueMessageCode as String)
+    }
+  }
+}
+
+extension MeViewController : UIPopoverPresentationControllerDelegate {
+  func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
+    return UIModalPresentationStyle.OverFullScreen
+  }
+  
+  func popoverPresentationControllerShouldDismissPopover(popoverPresentationController: UIPopoverPresentationController) -> Bool {
+    return true
+  }
+}
+
+extension MeViewController : UICollectionViewDelegate, UICollectionViewDataSource {
+  func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+    selectedContext(contextGroupCells[indexPath.row+1])
+  }
+  
+  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! MeCollectionViewCell
+    let contextGroupCellIndex = indexPath.row+1
+    let contextGroup = contextGroupCells[contextGroupCellIndex]
+    let overriddenContext = ContextInfo.sharedInstance.getOverriddenContext(contextGroup)
+    if overriddenContext.flag {
+      if let contextName = overriddenContext.contextName {
+        cell.imageView.image = UIImage(named: contextName.name.lowercaseString)
+        cell.contextLabel.text = contextName.name
+      } else if let userContextString = overriddenContext.userContextString {
+        cell.contextLabel.text = userContextString
+        cell.imageView.image = UIImage(named: "unknown")
+      } else {
+        //      Show loading image
+        cell.imageView.image = UIImage(named: "unknown")
+        cell.contextLabel.text = contextGroup.name
+      }
+    } else {
+      let currentContext = ContextInfo.sharedInstance.getValidCurrentContext(contextGroup)
+      if let
+        validContextName = currentContext.context,
+        validContextImageName = currentContext.imageName {
+        //      Send request for image
+        cell.imageView.image = UIImage(named: validContextImageName)
+        cell.contextLabel.text = contextGroup == NEContextGroup.Situation ?
+          ContextInfo.sharedInstance.getSituationDisplayMessage(validContextName.name) :
+          validContextName.name.name
+      } else {
+        //      Show loading image
+        cell.imageView.image = UIImage(named: "unknown")
+        cell.contextLabel.text = contextGroup.name
+      }
+    }
+    return cell
+  }
   
   func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    if collectionView == situationCollectionView {
-      return 1
-    } else {
-      return (contextGroupCells.count - 1)
-    }
+    return (contextGroupCells.count - 1)
   }
   
   func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize
   {
-    if collectionView == situationCollectionView {
-      let size : CGSize = CGSizeMake(collectionView.frame.size.width, collectionView.frame.size.width)
-      return size
-    } else {
-      let size : CGSize  = CGSizeMake(collectionView.frame.size.width/4-15, collectionView.frame.size.width/4+20-15)
-      return size
-    }
+    let size : CGSize  = CGSizeMake(collectionView.frame.size.width/4-15, collectionView.frame.size.width/4+20-15)
+    return size
   }
 }
