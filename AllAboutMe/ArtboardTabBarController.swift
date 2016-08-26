1cfb2532f1802511941b16076e5fba7856e39aa9
senabhishek <sen.abhishek@gmail.com>
2016-08-26 15:07:06 +0100

    Tab bar color changes. Remove month analytics. Insights bar 7 days only max.

--- AllAboutMe/ArtboardTabBarController.swift
+++ AllAboutMe/ArtboardTabBarController.swift
@@ -10,26 +10,28 @@ import UIKit
 
 class ArtboardTabBarController: UITabBarController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.tabBar.barTintColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.3)
+    self.tabBar.tintColor = UIColor.whiteColor()
+    self.tabBar.translucent = true
+    self.tabBar.opaque = false
+  }
 
-        // Do any additional setup after loading the view.
-    }
+  override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+  }
+  
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+  /*
+  // MARK: - Navigation
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+      // Get the new view controller using segue.destinationViewController.
+      // Pass the selected object to the new view controller.
+  }
+  */
 
 }
